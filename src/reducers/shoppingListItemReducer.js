2f09e3cee15664a58f58039b7e89990d9b2846f8
github username <scott.gloyna01@gmail.com>
2021-04-01 11:02:33 -0500

    Done.

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -6,11 +6,14 @@ export default function shoppingListItemReducer(
 ) {
 	switch (action.type) {
 		case 'INCREASE_COUNT':
+			console.log('Current state.items length %s', state.items.length)
+			console.log('updating state.items length to %s', state.items.length +1)
 			return {
 				...state,
 				items: state.items.concat(state.items.length + 1)
 			}
 		default:
+			console.log('initial state')
 			return state;
 	}
 }
